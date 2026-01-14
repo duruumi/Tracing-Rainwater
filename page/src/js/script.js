f8c9a963b04b83c364972902644fbdb086da9c0b
CHOMAE <76076856+jus0124@users.noreply.github.com>
2026-01-14 17:35:44 +0900

    Delete page directory

--- page/src/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("navbar-toggle");
-  const menu = document.getElementById("navbar-dropdown");
-  button.addEventListener("click", function () {
-    menu.classList.toggle("hidden");
-  });
-});
